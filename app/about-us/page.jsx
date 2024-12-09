63c877a644e104fb4503dc7dff84e64b3ab55259
kevinthomas <kevvin.thomas@gmail.com>
2024-12-09 22:17:19 +0530

    changes

--- app/about-us/page.jsx
+++ app/about-us/page.jsx
@@ -44,13 +44,15 @@ const page = () => {
             <h3 className={`${dmsans.className} text-[#d9d9d9] tracking-tight`}>Learn About What Drives Us</h3>
             <div className='w-40 md:w-80 bg-gradient-to-r from-[#101010] via-[#AE7C53] to-[#101010] h-[2px]'></div>
             <h1 className={`${federo.className} text-5xl md:text-6xl tracking-tight bg-gradient-to-r from-[#FFFFFF] to-[#AE7C53] inline-block text-transparent bg-clip-text`}>Our Mission</h1>
-            <p className={`${dmsans.className} text-white md:text-md text-sm tracking-tight md:w-1/2 text-center`}>Dedicated to redefining luxury, we craft timeless jewelry that embodies elegance, celebrates individuality, and creates cherished memories for generations to come.</p>
+            <p className={`${dmsans.className} text-white md:text-md text-sm tracking-tight md:px-28 text-center`}>Welcome to Q Stone Designs, a family owned jewelry manufacturing company with over 15 years of experience in creating timeless pieces. We are proud to be a 360° jewelry solution provider, supporting businesses from small retailers to large clients, offering everything from design to production. </p>
+            <p className={`${dmsans.className} text-white md:text-md text-sm tracking-tight md:px-28 text-center`}>Our extensive network of trusted contacts allows us to source the finest diamonds and gemstones and Precious Metals. Our in-house lab-grown diamond facility, launched in 2022, provides an ethical and sustainable alternative. Working with top designers from around the world, we bring your vision to life with unparalleled craftsmanship and creativity.</p>
+            <p className={`${dmsans.className} text-white md:text-md text-sm tracking-tight md:px-28 text-center`}>Beyond business, we are committed to giving back, supporting impactful initiatives, and promoting ethical practices within the jewelry industry.</p>
         </div>
         <div className='flex flex-col md:flex-row justify-between px-10 gap-10 md:gap-0 md:px-28 md:py-10'>
             {missions.map((mission) => {
                 return(
                     <div className='flex flex-col items-center gap-2 md:gap-5' key={mission}>
-                        <Image src={mission.icon} height={70}/>
+                        <Image src={mission.icon} className='md:h-16 h-16 object-contain'/>
                         <h3 className={`${dmsans.className} text-xl font-semibold tracking-tight`}>{mission.title}</h3>
                         <p className={`${dmsans.className} text-[#808080] text-center`}>{mission.desc}</p>
                     </div>
@@ -60,8 +62,7 @@ const page = () => {
         <div className='flex flex-col gap-3 items-center py-10'>
             <h1 className={`${federo.className} text-3xl tracking-tight text-[#F9C997] text-center`}>Why Choose Us?</h1>
             <div className='w-40 md:w-80 bg-gradient-to-r from-[#101010] via-[#AE7C53] to-[#101010] h-[2px]'></div>
-            <p className={`${dmsans.className} tracking-tight text-center w-3/4 pt-10`}>At Ipsum, we understand the demands and challenges of the jewelry industry. As a B2B partner, we focus on providing seamless, high-quality jewelry manufacturing and design services to jewelry companies around the world. From independent designers to large retail chains, we are dedicated to helping our clients scale their businesses with precision, efficiency, and a deep understanding of the market.
-            <br/>Our team of experts combines decades of experience in jewelry making, design, and sourcing, allowing us to offer innovative solutions that meet the highest standards. Whether you’re looking for rapid prototyping, mass production, or custom gemstone sourcing, we have the tools, expertise, and commitment to turn your ideas into reality.</p>
+            <p className={`${dmsans.className} tracking-tight text-center w-3/4 pt-10`}>At Q Stone Designs, our in-house lab-grown diamond facility and extensive network for sourcing natural stones enable us to offer you competitive rates on both diamonds and gemstones, all certified by IGI and GIA. We provide on-demand designers to collaborate with you or your clients to fulfill custom needs, while our worldwide shipping ensures hassle-free delivery. We support your business growth with cost-effective methods and advanced technology.</p>
         </div>
         <CustomFooter/>
     </div>
